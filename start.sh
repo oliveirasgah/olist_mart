a05b2fd9e9ed7305df41cc11cd93e6df870ca4d1
Gabriel Santos <oliveirasgah@gmail.com>
2026-04-01 10:06:34 -0300

    Create start.sh to deploy the application

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+RAW_DATA_PATH="${1:-raw_data}"
+
+echo "==> Installing dependencies..."
+poetry install
+
+echo "==> Loading raw data from '${RAW_DATA_PATH}'..."
+poetry run python load_raw.py "$RAW_DATA_PATH"
+
+echo "==> Installing dbt packages..."
+poetry run dbt deps
+
+echo "==> Running dbt models..."
+poetry run dbt run
+
+echo "==> Running dbt tests..."
+poetry run dbt test
+
+echo "Done."
